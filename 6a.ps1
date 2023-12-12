a764e63b5be19537f6cd3b34c372d770140d831f
katanakiwi <katanakiwi@gmail.com>
2023-12-12 15:27:10 +0100

    Add files via upload
    day 6

---
+++ 6a.ps1
@@ -0,0 +1,33 @@
+﻿$sample = $false
+$day = "6"
+
+if ($sample) {
+    $data = get-content ("$($PSScriptRoot)/$day"+"sample.txt")
+} else {
+    $data = get-content ("$($PSScriptRoot)/$day"+".txt")
+}
+
+
+$times = ($data[0].split(":") | Select-Object -skip 1).split(" ") | ? {$_ -ne ""}
+$dists = ($data[1].split(":") | Select-Object -skip 1).split(" ") | ? {$_ -ne ""}
+$nrOfRaces = $times.length
+$outPut = 1
+
+for ($i = 0; $i -lt $nrOfRaces; $i++) {
+    $distanceToBeat = [int]$dists[$i]
+    $speed = 0
+    $currentTime = 0
+    $maximumTime = [int]$times[$i]
+    $remainingTime = [int]$times[$i]
+    $amountOfRacesThatCanBeatDistance = 0
+
+    for ($j = 0; $j -le $times[$i]; $j++,$speed++,$remainingTime--) {
+        $distanceIfReleasedNow = $speed * $remainingTime
+        if ($distanceIfReleasedNow -gt $distanceToBeat) {
+            $amountOfRacesThatCanBeatDistance++
+        }
+    }
+    $output *= $amountOfRacesThatCanBeatDistance
+}
+
+write-host "output: $output"
---
+++ 6b.ps1
@@ -0,0 +1,41 @@
+﻿$sample = $false
+$day = "6"
+
+if ($sample) {
+    $data = get-content ("$($PSScriptRoot)/$day"+"sample.txt")
+} else {
+    $data = get-content ("$($PSScriptRoot)/$day"+".txt")
+}
+
+
+$time = [int64]($data[0].Replace(" ","").split(":") | select -skip 1)
+$distanceToBeat = [int64]($data[1].Replace(" ","").split(":") | select -skip 1)
+$amountOfRacesThatCanBeatDistance = 0
+$speed = 0
+$remainingTime = $time
+$firstRace = 0
+$lastRace = 0 
+
+for ($i = 0; $i -le $time; $i++, $speed++, $remainingTime--) {
+    $distanceIfReleasedNow = $speed * $remainingTime
+    if ($distanceIfReleasedNow -gt $distanceToBeat) {
+        $firstRace = $i
+        write-host "first $i"
+        break;
+    }
+}
+
+$remainingTime = 0
+$speed = $time
+for ($i = $time; $i -ge 0; $i--, $speed--, $remainingTime++) {
+    $distanceIfReleasedNow = $speed * $remainingTime
+    if ($distanceIfReleasedNow -gt $distanceToBeat) {
+        $lastRace = $i
+        write-host "first $i"
+        break;
+    }
+}
+
+$amountOfRacesThatCanBeatDistance = $lastRace - $firstRace +1
+
+write-host "amountOfRaces: $amountOfRacesThatCanBeatDistance"
